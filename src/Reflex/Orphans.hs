85c4f687c6492123c5758daaee399cde8eaf2416
davean <davean@xkcd.com>
2016-03-05 01:37:49 -0500

    Added Applicative instance for Dynamic.

--- src/Reflex/Orphans.hs
+++ src/Reflex/Orphans.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE LambdaCase #-}
 module Reflex.Orphans () where
 
+import Data.These
+import Data.Align
 import Reflex
 
 instance Reflex t => Functor (Dynamic t) where
     fmap f d = unsafeDynamic (fmap f . current $ d) (fmap f . updated $ d)
+
+instance Reflex t => Applicative (Dynamic t) where
+    pure = constDyn
+    f <*> x = unsafeDynamic
+              ((current f) <*> (current x))
+              (push (\case
+                        These a b -> return . Just $ a b
+                        _ -> return Nothing) $ align (updated f) (updated x))
--- test.hs
+++ test.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 import Test.Tasty
 import Test.Tasty.HUnit
 import Control.Monad
@@ -15,21 +16,60 @@ main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests" [
-    runSpiderTest "Test mapDyn" $ \ e -> do
+    runSpiderEBD "Test mapDyn" $ \ e -> do
       b <- hold 1 e
       d <- mapDyn show =<< holdDyn 1 e
       return (fmap show e, fmap show b, d)
-  , runSpiderTest "Test fmap Dynamic" $ \ e -> do
+  , runSpiderEBD "Test fmap Dynamic" $ \ e -> do
       b <- hold 1 e
       d <- holdDyn 1 e
       return (fmap show e, fmap show b, fmap show d)
+  , testApplicative
   ]
 
-runSpiderTest :: (Eq a, Show a)
+eventValue :: (Reflex t, MonadReadEvent t m) => EventHandle t a -> m (Maybe a)
+eventValue eh = readEvent eh >>= sequence
+
+sameBehavior :: (Reflex t, Eq a, MonadSample t m, MonadIO m, Show a) => Behavior t a -> Behavior t a -> m ()
+sameBehavior ba bb = do
+      va <- sample ba
+      vb <- sample bb
+      liftIO $ va @=? vb
+
+testApplicative :: TestTree
+testApplicative = testCase "Test Applicative" . runSpiderHost $ do
+    (re, rmt) <- newEventWithTriggerRef
+    (pb, pd) <- runHostFrame $ do
+      b <- hold 1 re
+      d <- holdDyn 1 re
+      return (show <$> b, show <$> d)
+    ehd <- subscribeEvent . updated $ pd
+    pb `sameBehavior` (current pd)
+    Just rt <- readRef rmt
+    forM_ [1..10] $ \nv -> do
+      checkStep =<< (fireEventsAndRead [rt :=> (Identity nv)] $ do
+        (,,,) <$> pure (Just . show $ nv)
+              <*> eventValue ehd
+              <*> sample pb
+              <*> (sample . current $ pd))
+      pb `sameBehavior` (current pd)
+    checkStep =<< (fireEventsAndRead [] $ do
+        (,,,) <$> pure Nothing
+              <*> eventValue ehd
+              <*> sample pb
+              <*> (sample . current $ pd))
+    pb `sameBehavior` (current pd)
+  where
+    eventValue eh = readEvent eh >>= sequence
+    checkStep (tv, ved, vba, vbb) = do
+      liftIO $ tv @=? ved
+      liftIO $ vba @=? vbb
+
+runSpiderEBD :: (Eq a, Show a)
         => TestName
         -> (Event Spider Int -> HostFrame Spider (Event Spider a, Behavior Spider a, Dynamic Spider a))
         -> TestTree
-runSpiderTest nm frm = testCase nm . runSpiderHost $ do
+runSpiderEBD nm frm = testCase nm . runSpiderHost $ do
     (re, rmt) <- newEventWithTriggerRef
     (pe, pb, pd) <- runHostFrame $ frm re
     ehe <- subscribeEvent pe
@@ -50,11 +90,6 @@ runSpiderTest nm frm = testCase nm . runSpiderHost $ do
                 <*> (sample . current $ pd))
     pb `sameBehavior` (current pd)
   where
-    eventValue eh = readEvent eh >>= sequence
     checkStep (vea, veb, vba, vbb) = do
       liftIO $ vea @=? veb
       liftIO $ vba @=? vbb
-    sameBehavior ba bb = do
-      va <- sample ba
-      vb <- sample bb
-      liftIO $ va @=? vb
