63331d4f86aad3efaefc2cee9413e15ae597346a
Luigy Leon <luichi.19@gmail.com>
2019-06-11 11:00:57 -0400

    Testing webhooks
    pull update

--- src/Reflex/Orphans.hs
+++ src/Reflex/Orphans.hs
@@ -7,8 +7,10 @@ import Data.AppendMap
 import qualified Data.Map as Map
 import Reflex
 
+
 instance (ToJSON k, ToJSON m) => ToJSON (AppendMap k m) where
   toJSON = toJSON . Map.toList . _unAppendMap
+
 
 instance (FromJSON k, FromJSON m, Ord k) => FromJSON (AppendMap k m) where
   parseJSON r = do
